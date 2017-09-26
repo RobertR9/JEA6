c1db054b5251c4548df51c3b31b3d4bda5fd2d1c
Robert Roubos <robertroubos9@gmail.com>
2017-09-26 19:30:17 +0200

    Remove unused code

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -17,39 +17,20 @@ import java.util.List;
 @Stateless
 public class UserService implements Serializable {
 
-    private User loggedInUser;
-
-    @Resource
-    private SessionContext sessionContext;
-
     @Inject
     private UserDAOJPAImpl userDAOJPAImpl;
     @Inject
     private KweetDAOJPAImpl kweetDAOJPAImpl;
 
-    public User getLoggedInUser() {
-//        if (loggedInUser == null) {
-//            Principal p = sessionContext.getCallerPrincipal();
-//            this.loggedInUser = userDAOJPAImpl.find(p.getName());
-//        }
-//
-//        return this.loggedInUser;
-        return null;
-    }
-
-    @PermitAll
     public List<User> getFollowers(User user) {
         return userDAOJPAImpl.findAllFollowers(user);
     }
 
-    @PermitAll
     public List<Kweet> getKweets(User user) {
         return kweetDAOJPAImpl.findTweetsByUser(user);
     }
 
-    //    @RolesAllowed("admin")
     public List<User> getAllUsers() {
         return userDAOJPAImpl.findAllUsers();
     }
-
 }
